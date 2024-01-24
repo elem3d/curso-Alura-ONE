58e89a5ea3fc7128ab147af21b5baaf70fe5b3f9
elem3d <146497742+elem3d@users.noreply.github.com>
2024-01-24 13:11:55 -0300

    numero-secreto-programado

--- Jogo-número-secreto/app.js
+++ Jogo-número-secreto/app.js
@@ -1,18 +1,24 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 a 10';
-
-
-
-function init() {
-    
-    function verificarChute() {
-        
-
-    }
-
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto
 }
+function gerarNumeroAleatório(){
+    parseInt(Math.random() * 10 + 1);
+}
+function init() {
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 a 10');
+    let numeroSecreto = gerarNumeroAleatório();
+    let chutes = 0;
 
+    function verificarChute() {
+        chutes++;
+        let chute = document.querySelector('input').value
+        if(chute == numeroSecreto){
+            alert(`Parabéns! Você acertou o número secreto com ${chutes} tentativas`);
+        } else {
+            alert('Você ainda não acertou o númeto secreto. Tente novamente.');
+        }
+    }
+}
 init();
